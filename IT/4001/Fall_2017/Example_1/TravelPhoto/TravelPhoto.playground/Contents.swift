ecd8b06c9b86c73bb12595b842e7d2fd49f26f27
Jackson Chandler <jackson.c.chandler@gmail.com>
2017-10-03 20:44:01 -0500

    Uploaded

---
+++ IT/4001/Fall_2017/Example_1/TravelPhoto/TravelPhoto.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+struct TravelPhoto{
+    let latitude: Double
+    let longitude: Double
+    let title: String
+    let description: String
+    let image: String
+    let urlString: String
+}
+
+let photo1 = TravelPhoto(latitude: 38.94733, longitude: -92.328466, title: "The Mizzou Columns", description: "University of Missouri six iconic columns", image: "columns.png", urlString: "http://missouri.edu/about/history/columns.php")
+
